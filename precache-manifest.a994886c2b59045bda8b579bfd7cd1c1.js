b886f04c904bdecf327e05084477b2c51d68657e
Lancaelot <Lancaelot@gmail.com>
2020-10-29 15:32:32 +0200

    Updates

---
+++ precache-manifest.a994886c2b59045bda8b579bfd7cd1c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2120ca9d9db574dbc8b536bc500974",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0dfb5bb716152e24f682",
+    "url": "./static/js/2.dc21eb97.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dc21eb97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be757db18ba4cb8006d",
+    "url": "./static/js/main.1b7ca90b.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6abdd38003b4e28b0c748d9b01572ff9.js"
+  "./precache-manifest.a994886c2b59045bda8b579bfd7cd1c1.js"
 );
 
 self.addEventListener('message', (event) => {
